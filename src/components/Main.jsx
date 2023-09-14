f850916d8d1ae5fe42540e1773fa3fa1e7b99422
GmoZen <zendejas.guillermo@gmail.com>
2023-09-13 23:28:21 -0700

    Add Main Component content and styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,16 @@ import React from "react"
 
 function Main() {
     return (
-        <h1>Main section goes here</h1>
+        <main>
+            <h1 className="main--tile">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was oriignally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
 
