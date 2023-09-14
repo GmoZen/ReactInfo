a6c2deccb163cd1eb75cf3a7111159f6e3de5369
GmoZen <zendejas.guillermo@gmail.com>
2023-09-13 22:22:03 -0700

    Create initial NavBar and Main compoments

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+function Main() {
+    return (
+        <h1>Main section goes here</h1>
+    )
+}
+
+export default Main
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function NavBar() {
+    return (
+        <nav>
+            <h1>NavBar goes here</h1>
+        </nav>
+    )
+}
+
+
+export default NavBar
